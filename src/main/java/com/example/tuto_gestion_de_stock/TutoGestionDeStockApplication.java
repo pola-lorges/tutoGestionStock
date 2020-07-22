bf2d08078cdd8c8518d4a88be36812e29435b5a1
Anthony POLA <anthonypola100@gmail.com>
2020-07-22 11:06:37 +0100

    premier commit

---
+++ src/main/java/com/example/tuto_gestion_de_stock/TutoGestionDeStockApplication.java
@@ -0,0 +1,13 @@
+package com.example.tuto_gestion_de_stock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TutoGestionDeStockApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TutoGestionDeStockApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/tuto_gestion_de_stock/TutoGestionDeStockApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tuto_gestion_de_stock;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TutoGestionDeStockApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
